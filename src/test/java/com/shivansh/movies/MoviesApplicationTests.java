32ee98b1d5d56093e8b2db846f9db35303d14561
shivanshhgit <shivanshbansal21@gmail.com>
2025-01-17 18:55:02 +0530

    Initialization

---
+++ src/test/java/com/shivansh/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivansh.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
